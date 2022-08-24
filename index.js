1c53a00b3481670a1198d72983529be73216eeb5
jatinKamboj <kumar.jatin873@gmail.com>
2022-08-24 21:15:00 +0530

    configure env file

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ const PORT = process.env.PORT || 3000;
 
 const app = express();
 
+require('dotenv').config();
+
+app.get('/', (req, res) => {
+    res.send(process.env.URL);
+});
+
 app.listen(PORT, () => {
     console.log(`Server is running at ${PORT} port`);
 });
